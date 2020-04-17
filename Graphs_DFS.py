37af34de96035bc93bada1b3d061ca4bbd098c58
dan <danieldom0607@gmail.com>
2020-04-16 20:29:57 -0400

    Minor fixes

--- Graphs_DFS.py
+++ Graphs_DFS.py
@@ -10,11 +10,11 @@ def import_data(f_name):
         print('Could not find file!')
 
 #Node class for each airport
-class Node():
-    def __init__(self, name, dest = "", dist = 0):
+class Node:
+    def __init__(self, name):
         self.name = name 
-        self.dest = dest
-        self.dist = dist
+        self.dest = ""
+        self.dist = 0
         self.edges = {}
         self.visited = False
     
@@ -23,22 +23,19 @@ class Node():
     def add_edge(self, _edge):
         self.edges.update(_edge)
     
-    #This is a function to return the nearest edge of the given node
+    #This is a function to return the nearest edge
     def min_edge(self):
         edge_keys = sorted(self.edges)
         return self.edges.get(edge_keys[0])
 
-#Graph class to create the display 
-class Graph():
+#Graph class to structure nodes
+class Graph:
     def __init__(self, data):
         self.data = data
-        #nodes is a dictionary where the key is the node name and the value is the node object
         self.nodes = {}
-        self.known_nodes = []
 
     #Functiont to add a node to the graph or edge to an existing edge
     def add_nodes(self):
-        node_count = 0
         for line in self.data:
             _name = line[:3]
             _dest = line[4:7]
@@ -47,12 +44,9 @@ class Graph():
             new_node = Node(_name)
             #If the node is not known we create it and add the first edge. If the node is known, we reference it and add the edge.
             #An edge is a dict where the key is the distance and the destination is the value 
-            if _name not in self.known_nodes:
+            if _name not in self.nodes.keys():
                 self.nodes.update( { _name : new_node} )
-                #Add the first edge
                 self.nodes[_name].add_edge(_edge)
-                self.known_nodes.append(_name)
-                node_count+=1
             else:
                 self.nodes[_name].add_edge(_edge)
 
@@ -60,7 +54,6 @@ class Graph():
             # Create a queue for BFS
             queue = []
             path = ""
-            total_time = 0
             # Mark the source node as
             # visited and enqueue it
             queue.append(self.nodes[origin])
@@ -84,7 +77,7 @@ class Graph():
                     if _curr_edge.visited == False:
                         _curr_edge.visited = True
                         queue.append(_curr_edge)
-            print(path)
+            return path
                     
 if __name__ == '__main__':
 
@@ -93,5 +86,5 @@ if __name__ == '__main__':
     ap_graph = Graph( data )
     ap_graph.add_nodes()
 
-    ap_graph.BFS('ACY', 'BDL')
+    print(ap_graph.BFS('ACY', 'BDL'))
     
